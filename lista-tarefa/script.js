585a236110f6b574fddeeeeafcf59bf38f4e64eb
Weber-Paulo <paulomorandini@gmail.com>
2022-09-01 22:25:10 -0300

    atualizando

--- lista-tarefa/script.js
+++ lista-tarefa/script.js
@@ -1,13 +1,25 @@
 const inputTarefa = document.getElementById("tarefa-nova")
 
-function adicionarTarefa(){
-    const item = document.createElement("li")
+function adicionarTarefa() {
+    const item = document.createElement("li");
 
-    item.textContent = "Ola"
+    item.innerHTML = `<li>
+    <span>${inputTarefa.value}</span>
+    <button class="excluir">x</button>
+</li>`;
 
-    const lista = document.querySelector("#lista")    
-    lista.appendChild(item)
+    const lista = document.querySelector("#lista")
+    lista.appendChild(item);
 
+    inputTarefa.value = ""
+}
+const botaoAdicionar = document.querySelector("#btn-adicionar")
+botaoAdicionar.addEventListener("click", adicionarTarefa)
+
+function limparLista(){
+    const lista = document.querySelector("#lista");
+    lista.innerHTML = ""
 }
 
-adicionarTarefa()
+const botaoLimpar = document.querySelector(".bt-azul")
+botaoLimpar.addEventListener("click", limparLista)
